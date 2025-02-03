993dd6e5045ecd376f694627a4bdc614ce46d3ad
AbdullahHishamK <162813766+AbdullahHishamK@users.noreply.github.com>
2025-02-03 02:26:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
-// import "./../node_modules/google-fonts/demo.js"
 import './index.css'
 import App from './App.jsx'
 
